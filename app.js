f2fdef0781062833e35c17e352fe1ba34e4dfa67
Travis Pett <travispett@gmail.com>
2017-11-05 15:07:35 -0500

    Scaffold gifs controller

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const path = require('path');
 const express = require('express');
+const router = express.Router();
 const app = express();
+const PORT = process.env.API_PORT || 3000;
 
-const PORT = process.env.PORT || 8080;
+const GifsController = require('./app/controllers/GifsController');
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+router.get('/gifs', GifsController.index);
+
+// Mount routes at /api
+app.use('/api', router);
+// Raise 404 if a route with a controller is not hit.
+app.use((req, res, next) => res.sendStatus(404));
+// TODO: console.log is bad in prod, replace with winston, etc.
 app.listen(PORT, () => console.log(`API started on port ${PORT}`));
---
+++ app/controllers/GifsController.js
@@ -0,0 +1,7 @@
+function index(req, res) {
+  res.send('ok');
+};
+
+module.exports = {
+  index
+};
